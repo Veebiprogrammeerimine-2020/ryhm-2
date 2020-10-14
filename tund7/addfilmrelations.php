d050932bb200b999a522698a55690816416d06e9
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-10-14 12:27:49 +0300

    Kuues kodutöö tehtud

--- tund7/addfilmrelations.php
+++ tund7/addfilmrelations.php
@@ -1,17 +1,5 @@
 <?php
-  session_start();
-  
-  //kui pole sisseloginud
-  if(!isset($_SESSION["userid"])){
-	  //jõugu sisselogimise lehele
-	  header("Location: page.php");
-  }
-  //väljalogimine
-  if(isset($_GET["logout"])){
-	  session_destroy();
-	   header("Location: page.php");
-	   exit();
-  }
+  require("usesession.php");
   //loeme andmebaasi login ifo muutujad
   require("../../../../config_vp2020.php");
   require("fnc_filmrelations.php");
@@ -45,7 +33,6 @@
   require("header.php");
 ?>
 
-  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse bänner">
   <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?> programmeerib veebi</h1>
   <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
