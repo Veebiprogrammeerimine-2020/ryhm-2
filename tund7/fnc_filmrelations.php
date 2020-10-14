ef123def673f06562be343903ed6dd514b151131
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-10-14 14:37:19 +0300

    Seitsmes tuind, alustame sorteerimist

--- tund7/fnc_filmrelations.php
+++ tund7/fnc_filmrelations.php
@@ -135,6 +135,34 @@ function storenewstudiorelation($selectedfilm, $selectedstudio){
 }
 
 function readpersoninmovie(){
+	$notice = "<p>Kahjuks ei leidnud filmitegelasi!</p>";
+	$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
+	$stmt=$conn->prepare("SELECT first_name, last_name, role, title FROM person JOIN person_in_movie ON person.person_id = person_in_movie.person_id JOIN movie ON movie.movie_id = person_in_movie.movie_id");
+	echo $conn->error;
+	$stmt->bind_result($firstnamefromdb, $lastnamefromdb, $rolefromdb, $titlefromdb);
+	$stmt->execute();
+	$lines = "";
+	while($stmt->fetch()){
+		$lines .= "\t <tr> \n";
+		$lines .= "\t \t <td>" .$firstnamefromdb ." " .$lastnamefromdb ."</td>";
+		$lines .= "<td>" .$rolefromdb ."</td>";
+		$lines .= "<td>" .$titlefromdb ."</td> \n";
+		$lines .= "\t </tr> \n";
+	}
+	if(!empty($lines)){
+		$notice = "<table> \n";
+		$notice .= "\t <tr> \n \t \t <th>Isik</th> \n \t \t <th>Roll</th> \n";
+		$notice .= "\t \t" .'<th>Film <a href="?sortby=4&sortorder=1">&uarr;</a>&nbsp;<a href="?sortby=4&sortorder=2">&darr;</a></th>' ."\n \t </tr> \n";
+		$notice .= $lines;
+		$notice .= "</table> \n";
+	}
+	
+	$stmt->close();
+	$conn->close();
+	return $notice;
+}
+
+function old_version_readpersoninmovie(){
 	$notice = "";
 	$conn = new mysqli($GLOBALS["serverhost"], $GLOBALS["serverusername"], $GLOBALS["serverpassword"], $GLOBALS["database"]);
 	$stmt=$conn->prepare("SELECT first_name, last_name, role, title FROM person JOIN person_in_movie ON person.person_id = person_in_movie.person_id JOIN movie ON movie.movie_id = person_in_movie.movie_id");
