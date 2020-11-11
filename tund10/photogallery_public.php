fb6ba2995019cdb85f83b45892de5f9173f5bb79
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-11-11 15:04:47 +0200

    Kümnes tund käib, galerii toimimas

--- tund10/photogallery_public.php
+++ tund10/photogallery_public.php
@@ -8,7 +8,20 @@
   $fileuploaddir_normal = "../photoupload_normal/";
   $fileuploaddir_thumb = "../photoupload_thumb/";
   
-  $publicphotothumbshtml = readAllPublicPhotoThumbs(2);
+  $gallerypagelimit = 3;
+  $page = 1;
+  $photocount = countPublicPhotos(2);
+  
+  if(!isset($_GET["page"]) or $_GET["page"] < 1){
+	  $page = 1;
+  } elseif((intval($_GET["page"]) - 1) * $gallerypagelimit >= $photocount){
+	  $page = ceil($photocount/$gallerypagelimit);
+  } else {	  
+	  $page = intval($_GET["page"]);
+  }
+  
+  //$publicphotothumbshtml = readAllPublicPhotoThumbs(2);
+  $publicphotothumbshtml = readAllPublicPhotoThumbsPage(2, $gallerypagelimit, $page);
     
   require("header.php");
 ?>
@@ -21,6 +34,20 @@
   </ul>
   <hr>
   <h2>Fotogalerii</h2>
+  <p>
+	<?php
+		if($page > 1){
+			echo '<span><a href="?page=' .($page - 1) .'">Eelmine leht</a></span> | ' ."\n";
+		} else {
+			echo '<span>Eelmine leht</span> | ' ."\n";
+		}
+		if($page * $gallerypagelimit < $photocount){
+			echo '<span><a href="?page=' .($page + 1) .'">Järgmine leht</a></span>' ."\n";
+		} else {
+			echo '<span>Järgmine leht</span>' ."\n";
+		}
+	?>
+  </p>
   <?php
 	echo $publicphotothumbshtml;
   ?>
